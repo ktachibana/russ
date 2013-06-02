3b18f8e439e39a7fe61de6b06c625bf24f830662
Kenichi Tachibana <deadzone@be.to>
2013-06-02 13:48:38 +0900

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Russ::Application.config.secret_key_base = '307c1c503a3218ce069560ccd621039ec5100b267fdf64f5ad983d33e94c8b34490f012bcc9be6d9f8b982046f420c07a99de76e0dfce37a3a5ca60eecd2863c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Russ::Application.config.session_store :cookie_store, key: '_russ_session'
