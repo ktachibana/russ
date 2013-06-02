4b05fb8cc1da44a2314197b4c06c5103e86ba81c
Kenichi Tachibana <deadzone@be.to>
2013-06-03 01:43:33 +0900

    DBのカラムサイズが足りなかった

--- db/migrate/20130602072509_create_feeds.rb
+++ db/migrate/20130602072509_create_feeds.rb
@@ -2,10 +2,10 @@ class CreateFeeds < ActiveRecord::Migration
   def change
     create_table :feeds do |t|
       t.references :user, index: true, null: false
-      t.string :url, null: false
-      t.string :title, null: false
-      t.string :link_url
-      t.text :description
+      t.string :url, null: false, limit: 2048
+      t.string :title, null: false, limit: 255
+      t.string :link_url, limit: 2048
+      t.text :description, limit: 4096
 
       t.timestamps
     end
