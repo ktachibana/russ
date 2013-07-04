1cba14f89aea027312730bad980b106bd7c17b7a
Kenichi Tachibana <deadzone@be.to>
2013-07-04 23:58:09 +0900

    unicorn入れたりcapifyしたりデプロイ準備

--- Gemfile
+++ Gemfile
@@ -36,12 +36,6 @@ end
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano', group: :development
-
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
@@ -73,3 +67,6 @@ gem 'devise', github: 'plataformatec/devise', branch: 'rails4'
 gem 'haml'
 gem 'kaminari'
 gem 'whenever', require: false, github: 'iTakeshi/whenever', branch: 'rails4'
+gem 'capistrano'
+gem 'capistrano_colors'
+gem 'unicorn'
---
+++ config/deploy.rb
@@ -0,0 +1,21 @@
+set :application, 'russ'
+set :repository,  'https://github.com/ktachibana/russ.git'
+set :scm, :git
+set :deploy_via, :copy
+
+server "192.168.0.6", :web, :app, :db
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
