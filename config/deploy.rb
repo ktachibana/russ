f3ba3d5b24283a50827e6d6810749ccc7c8f99b6
Kenichi Tachibana <deadzone@be.to>
2013-07-09 01:13:23 +0900

    capistranoでwhenever自動update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-require "bundler/capistrano"
+require 'bundler/capistrano'
 require 'capistrano-rbenv'
 set :application, 'russ'
 set :repository, 'https://github.com/ktachibana/russ.git'
@@ -13,6 +13,8 @@ default_run_options[:pty] = true
 
 server "192.168.0.6", :web, :app, :db, primary: true
 
+require 'whenever/capistrano'
+set :whenever_command, 'bundle exec whenever'
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
