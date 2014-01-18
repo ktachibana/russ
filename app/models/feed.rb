dc7ce9da1c6c5cd9e4310fc790b83f528fceafbf
Kenichi Tachibana <deadzone@be.to>
2014-01-18 22:00:32 +0900

    Feedのファイル読み込みをサブモジュールに

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -15,52 +15,65 @@ class Feed < ActiveRecord::Base
   validates :link_url, presence: true, length: { maximum: 2048 }
   validates :description, length: { maximum: 4096 }
 
-  def self.by_url(url)
-    load_rss(url) do |rss|
-      new do |m|
-        m.url = url
-        m.update_by_rss!(rss)
+  module FileLoadable
+    extend ActiveSupport::Concern
+
+    def update_by_rss!(rss)
+      self.title = rss.channel.title
+      self.description = rss.channel.description
+      self.link_url = rss.channel.link
+    end
+
+    def load!
+      self.class.load_rss(url) do |rss|
+        update_by_rss!(rss)
+        save
+
+        rss.items.each do |loaded_item|
+          guid = loaded_item.try(:guid).try(:content)
+
+          item = guid && items.find_by(guid: guid)
+          item ||= items.find_by(link: loaded_item.link)
+          item ||= items.build
+          item.link = loaded_item.link
+          item.title = loaded_item.title
+          item.guid = guid
+          item.published_at = loaded_item.date
+          item.description = loaded_item.description
+          item.save
+        end
+      end
+    rescue => e
+      Rails.logger.error(e)
+      nil # TODO エラーハンドリング
+    end
+
+    module ClassMethods
+      def by_url(url)
+        load_rss(url) do |rss|
+          new do |m|
+            m.url = url
+            m.update_by_rss!(rss)
+          end
+        end
+      end
+
+      def load_all!
+        logger.info('start load_all!')
+        find_each do |feed|
+          sleep(5)
+          feed.load!
+        end
+      end
+
+      def load_rss(url)
+        open(url) do |rss_body|
+          return yield RSS::Parser.parse(rss_body)
+        end
       end
     end
   end
-
-  def update_by_rss!(rss)
-    self.title = rss.channel.title
-    self.description = rss.channel.description
-    self.link_url = rss.channel.link
-  end
-
-  def self.load_all!
-    logger.info('start load_all!')
-    find_each do |feed|
-      sleep(5)
-      feed.load!
-    end
-  end
-
-  def load!
-    self.class.load_rss(url) do |rss|
-      update_by_rss!(rss)
-      save
-
-      rss.items.each do |loaded_item|
-        guid = loaded_item.try(:guid).try(:content)
-
-        item = guid && items.find_by(guid: guid)
-        item ||= items.find_by(link: loaded_item.link)
-        item ||= items.build
-        item.link = loaded_item.link
-        item.title = loaded_item.title
-        item.guid = guid
-        item.published_at = loaded_item.date
-        item.description = loaded_item.description
-        item.save
-      end
-    end
-  rescue => e
-    Rails.logger.error(e)
-    nil # TODO エラーハンドリング
-  end
+  include FileLoadable
 
   def self.import!(user, opml)
     doc = REXML::Document.new(opml)
@@ -90,12 +103,5 @@ class Feed < ActiveRecord::Base
       end
     end
     result
-  end
-
-  private
-  def self.load_rss(url)
-    open(url) do |rss_body|
-      return yield RSS::Parser.parse(rss_body)
-    end
   end
 end
