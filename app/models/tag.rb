2fae51f9d528c46b9d5fa67455adb689dcff89a9
Kenichi Tachibana <deadzone@be.to>
2014-01-20 01:38:04 +0900

    自前のタグ関係のモデルを削除

--- app/models/tag.rb
+++
@@ -1,3 +0,0 @@
-class Tag < ActiveRecord::Base
-  validates :name, presence: true, length: { maximum: 255 }
-end
--- spec/models/tag_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe Tag do
-  describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should ensure_length_of(:name).is_at_most(255) }
-  end
-end
