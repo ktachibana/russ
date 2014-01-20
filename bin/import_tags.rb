f318c38da25c3a46a8cc3488dd3afaf3da6c440e
Kenichi Tachibana <deadzone@be.to>
2014-01-21 01:02:25 +0900

    production環境でtappは使えないので

--- bin/import_tags.rb
+++ bin/import_tags.rb
@@ -2,12 +2,12 @@ json = JSON.load ARGF.read
 
 json['tags'].each do |tag|
   attrs = { name: tag['name'] }
-  ActsAsTaggableOn::Tag.create(attrs).tapp
+  p ActsAsTaggableOn::Tag.create(attrs)
 end
 json['taggings'].each do |tagging|
   attrs = { tag_id: tagging['tag_id'],
             taggable: Feed.find(tagging['feed_id']),
             context: 'tags',
             created_at: tagging['created_at'] }
-  ActsAsTaggableOn::Tagging.create(attrs).tapp
+  p ActsAsTaggableOn::Tagging.create(attrs)
 end
