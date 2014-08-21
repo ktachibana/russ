2c4c1236c533def3b8bb7dfd9a11f389a79a66d1
Kenichi Tachibana <deadzone@be.to>
2014-08-22 01:43:36 +0900

    reverse proxyを有効にするためにassets precompileを行う

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,17 @@ set :whenever_command, 'bundle exec whenever'
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
+namespace :assets do
+  task :precompile, :roles => :web, :except => { :no_release => true } do
+    from = source.next_revision(current_revision)
+    if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+      run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+    else
+      logger.info "Skipping asset pre-compilation because there were no asset changes"
+    end
+  end
+end
+
 namespace :deploy do
   [:start, :stop, :restart].each do |action|
     desc "#{action} application server."
