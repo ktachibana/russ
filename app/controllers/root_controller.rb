a1119e165237aeabb93a82f5aa0981c3e2e27b41
Kenichi Tachibana <deadzone@be.to>
2014-08-30 21:14:12 +0900

    不要なログを削除

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,6 +1,5 @@
 class RootController < ApplicationController
   def index
-    logger.info(MultiJson.engine)
     @items = Item.search(current_user, params)
     @tags = current_user.subscriptions.tag_counts
   end
