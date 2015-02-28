8ed389f402ae4d5295ab3276d9641448141796cd
Kenichi Tachibana <deadzone@be.to>
2015-02-28 15:14:24 +0900

    JSON APIのValidationエラーのレスポンスを統一

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+  private
+
+  def record_invalid(error)
+    respond_to do |f|
+      f.json do
+        render(json: { type: 'validation', errors: error.record.errors },
+               status: :unprocessable_entity) # Railsのデフォルトに従う。Roy Fieldingも言っている。
+      end
+      f.any { fail error }
+    end
+  end
 end
---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+RSpec.describe ApplicationController, type: :controller do
+  describe '.rescue_from' do
+    describe 'ActiveRecord::RecordInvalid' do
+      controller do
+        skip_before_filter :authenticate_user!
+
+        def index
+          User.create!
+        end
+      end
+
+      it 'HTMLをリクエストしたときはHTMLでエラーを返す' do
+        expect { get :index }.to raise_error(ActiveRecord::RecordInvalid)
+        expect(response.content_type).to start_with('text/html')
+      end
+
+      it 'JSONをリクエストしたときはJSONでエラーを返す' do
+        request.accept = 'application/json'
+        get :index
+        is_expected.to respond_with(:unprocessable_entity)
+        expect(response.content_type).to start_with('application/json')
+        parsed = JSON.parse(response.body)
+        expect(parsed).to eq('type' => 'validation',
+                             'errors' => { 'email' => ['入力してください'], 'password' => ['入力してください'] })
+      end
+    end
+  end
+end
