2bcba3afea5c0eff9674e868e9d66eda71a14956
Kenichi Tachibana <deadzone@be.to>
2015-03-08 23:55:57 +0900

    不要なeager loadingを消した

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,12 +2,12 @@ class FeedsController < ApplicationController
   def index
     respond_to do |f|
       f.html
-      f.json { @subscriptions = owned_subscriptions.preload({ feed: :latest_item }).order(:id).search(params) }
+      f.json { @subscriptions = owned_subscriptions.preload(feed: :latest_item).order(:id).search(params) }
     end
   end
 
   def show
-    @subscription = owned_subscriptions.includes(feed: :items).find_by!(feed_id: params[:id])
+    @subscription = owned_subscriptions.find_by!(feed_id: params[:id])
     @items = @subscription.feed.items.page(params[:page])
   end
 
