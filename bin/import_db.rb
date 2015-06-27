8b237bcde5b9fa172155b5eed15cb2d915ffbe9e
Kenichi Tachibana <deadzone@be.to>
2015-06-28 00:31:35 +0900

    tapp削除

--- bin/import_db.rb
+++ bin/import_db.rb
@@ -1,17 +1,17 @@
 dir = Rails.root + 'tmp' + 'exports'
 dir.children.each do |path|
   next unless path.fnmatch?('*.json')
-  klass = path.basename('.json').to_s.tapp.constantize
+  klass = path.basename('.json').to_s.constantize
 
   buffer = []
   path.each_line do |line|
     buffer << klass.new(JSON.parse(line.strip))
     if 1000 <= buffer.size
       klass.import(buffer, validate: false)
-      buffer.map(&:id).join(',').tapp
+      buffer.map(&:id).join(',')
       buffer.clear
     end
   end
   klass.import(buffer, validate: false) if buffer.present?
-  buffer.map(&:id).join(',').tapp
+  buffer.map(&:id).join(',')
 end
