ab7741f1df9db0fb9beb39acbc309b4e4993881b
Kenichi Tachibana <deadzone@be.to>
2016-06-25 12:04:43 +0900

    NODE_ENVをDefinePluginで展開して、production/developmentのビルドを分けた

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,3 +1,4 @@
+var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
@@ -14,5 +15,10 @@ module.exports = {
   },
   resolve: {
     extensions: ['', '.webpack.js', '.web.js', '.coffee', '.jsx', '.js']
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    })
+  ]
 };
