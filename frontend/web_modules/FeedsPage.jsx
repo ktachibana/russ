22dca57ddc9fd697b0187e16a537caec97c1c80f
Kenichi Tachibana <deadzone@be.to>
2021-08-07 23:00:55 +0900

    FeedsPageをfunction化

--- frontend/web_modules/FeedsPage.jsx
+++ frontend/web_modules/FeedsPage.jsx
@@ -1,86 +1,70 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {withRouter} from 'react-router-dom';
 import TagButtons from 'TagButtons';
 import WithPagination from 'WithPagination';
-import api from 'Api';
 import {SubscriptionRow} from './SubscriptionRow'
-
-class FeedsPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      tags: [],
-      subscriptions: [],
-      pagination: null
-    };
-  }
-
-  get currentTags() {
-    return this.selectTags(this.props.currentTagNames);
-  }
-
-  selectTags(tagNames) {
-    return this.state.tags.filter(tag => tagNames.includes(tag.name));
-  }
-
-  updateFeeds(props) {
-    const query = {
-      page: props.page,
-      tag: props.currentTagNames
-    };
-    api.loadFeeds(query).then(({subscriptions, pagination}) => {
-      this.setState({
-        subscriptions: subscriptions,
-        pagination: pagination
-      });
-    });
-  }
-
-  componentDidMount() {
-    api.loadInitial().then((data) => {
-      this.setState({tags: data.tags});
-    });
-    this.updateFeeds(this.props);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.updateFeeds(nextProps);
-  }
-
-  changeUrl({page = this.props.page, currentTagNames = this.props.currentTagNames}) {
-    const tagParam = currentTagNames.map(tag => encodeURIComponent(tag)).join(',');
-    this.props.history.push(`/feeds/${page}/${tagParam}`);
-  }
-
-  pagenationChanged(newPage) {
-    this.changeUrl({page: newPage});
-  }
-
-  tagButtonsChanged(newTags) {
-    this.changeUrl({page: 1, currentTagNames: newTags.map(tag => tag.name)});
-  }
-
-  render() {
-    return (
-      <div>
-        <TagButtons tags={this.state.tags} currentTags={this.currentTags} onChange={this.tagButtonsChanged.bind(this)}/>
-        <hr/>
-
-        <WithPagination pagination={this.state.pagination}
-                        currentPage={this.props.page}
-                        onPageChange={this.pagenationChanged.bind(this)}>
-          <div className='feeds'>
-            {this.state.subscriptions.map(subscription =>
-              <div key={subscription.id}>
-                <SubscriptionRow subscription={subscription}/>
-              </div>
-            )}
-          </div>
-        </WithPagination>
-      </div>
-    );
-  }
-}
+import api from 'Api';
 
 export default withRouter(FeedsPage);
+
+function FeedsPage({page, currentTagNames, history}) {
+  const [tags, setTags] = useState([]);
+  const [subscriptions, setSubscriptions] = useState([]);
+  const [pagination, setPagination] = useState(null)
+
+  const currentTags = tags.filter(tag => currentTagNames.includes(tag.name));
+
+  function updateFeeds() {
+    api.loadFeeds({
+      page: page,
+      tag: currentTagNames
+    }).then(({subscriptions, pagination}) => {
+      setSubscriptions(subscriptions);
+      setPagination(pagination);
+    });
+  }
+
+  useEffect(() => {
+    api.loadInitial().then(({tags}) => {
+      setTags(tags);
+    });
+  }, []);
+
+  useEffect(() => {
+    updateFeeds();
+  }, [page, currentTagNames]);
+
+  const changeUrl = ({newPage = page, newTagNames = currentTagNames}) => {
+    const tagParam = newTagNames.map(tag => encodeURIComponent(tag)).join(',');
+    history.push(`/feeds/${newPage}/${tagParam}`);
+  }
+
+  const pagenationChanged = newPage => {
+    changeUrl({newPage: newPage});
+  }
+
+  const tagButtonsChanged = newTags => {
+    changeUrl({newPage: 1, newTagNames: newTags.map(tag => tag.name)});
+  }
+
+  return (
+    <div>
+      <TagButtons tags={tags} currentTags={currentTags} onChange={tagButtonsChanged}/>
+      <hr/>
+
+      <WithPagination
+        pagination={pagination}
+        currentPage={page}
+        onPageChange={pagenationChanged}
+      >
+        <div className='feeds'>
+          {subscriptions.map(subscription =>
+            <div key={subscription.id}>
+              <SubscriptionRow subscription={subscription}/>
+            </div>
+          )}
+        </div>
+      </WithPagination>
+    </div>
+  );
+}
